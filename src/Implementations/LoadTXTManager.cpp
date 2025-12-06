cabe349f824600e6745579b8b75b638cd3dfa519
Scarlet002 <talikszymon@gmail.com>
2025-12-06 21:07:31 +0100

    Add files via upload

--- src/Implementations/LoadTXTManager.cpp
+++ src/Implementations/LoadTXTManager.cpp
@@ -24,17 +24,65 @@ void LoadTXTManager::LoadGameState(GameState& gameState, const string& fileName)
             return;
         }
 
-        int bullet, hpHuman, hpComputer, full, empty, bulletCount, Starter, difficulty, choice;
-        gameSave >> hpHuman >> hpComputer >> full >> empty >> bulletCount >> Starter >> difficulty >> choice;
+        int bullet, hpHuman, itemsHuman, sawsHuman, beersHuman, magnifiersHuman, invertersHuman, handCuffsHuman, cellPhonesHuman,
+            hpComputer, itemsComputer, sawsComputer, beersComputer, magnifiersComputer, invertersComputer, handCuffsComputer, cellPhonesComputer,
+            full, empty, bulletCount, starter, difficulty, choice, shooter, target, item, itemHuman, itemComputer, damage; 
+        bool wereHandCuffsUsed;
+
+        gameSave >> hpHuman >> itemsHuman;
 
         gameState.human.SetHP(hpHuman);
+        vector<int> newHumanInventory;
+        for (int i = 0; i < itemsHuman; i++)
+        {
+            gameSave >> itemHuman;
+            newHumanInventory.push_back(itemHuman);
+        }
+
+        gameSave >> sawsHuman >> beersHuman >> magnifiersHuman >> invertersHuman >> handCuffsHuman >> cellPhonesHuman;
+
+        gameState.human.SetInventory(newHumanInventory);
+        gameState.human.SetSaws(sawsHuman);
+        gameState.human.SetBeers(beersHuman);
+        gameState.human.SetMagnifiers(magnifiersHuman);
+        gameState.human.SetInverters(invertersHuman);
+        gameState.human.SetHandCuffs(handCuffsHuman);
+        gameState.human.SetCellPhones(cellPhonesHuman);
+
+        gameSave >> hpComputer >> itemsComputer;
+
         gameState.computer.SetHP(hpComputer);
+        vector<int> newComputerInventory;
+        for (int i = 0; i < itemsComputer; i++)
+        {
+            gameSave >> itemComputer;
+            newComputerInventory.push_back(itemComputer);
+        }
+
+        gameSave >> sawsComputer >> beersComputer >> magnifiersComputer >> invertersComputer >> handCuffsComputer >> cellPhonesComputer;
+
+        gameState.computer.SetInventory(newComputerInventory);
+        gameState.computer.SetSaws(sawsComputer);
+        gameState.computer.SetBeers(beersComputer);
+        gameState.computer.SetMagnifiers(magnifiersComputer);
+        gameState.computer.SetInverters(invertersComputer);
+        gameState.computer.SetHandCuffs(handCuffsComputer);
+        gameState.computer.SetCellPhones(cellPhonesComputer);
+
+        gameSave >> full >> empty >> bulletCount >> starter >> difficulty >> choice >> shooter >> target >> item >> damage >> wereHandCuffsUsed;
+
         gameState.magazine.SetFull(full);
         gameState.magazine.SetEmpty(empty);
         gameState.magazine.SetBulletCount(bulletCount);
-        gameState.gameStateManager.SetStarter(Starter);
+
+        gameState.gameStateManager.SetStarter(starter);
         gameState.ai.SetDifficulty(difficulty);
         gameState.gameStateManager.SetChoice(choice);
+        gameState.gameStateManager.SetShooter(shooter);
+        gameState.gameStateManager.SetTarget(target);
+        gameState.gameStateManager.SetItem(item);
+        gameState.gameStateManager.SetDamage(damage);
+        gameState.gameStateManager.SetStateOfHandCuffs(wereHandCuffsUsed);
 
         vector<int> newMagazine;
         for (int i = 0; i < bulletCount; i++)
--- src/Implementations/SaveTXTManager.cpp
+++ src/Implementations/SaveTXTManager.cpp
@@ -24,14 +24,47 @@ void SaveTXTManager::SaveGameState(const GameState& gameState, const string& fil
             cout << "Blad podczas zapisu stanu gry!" << endl;
             return;
         }
+
         gameSave << gameState.human.GetHP() << endl;
+        int itemsHuman = gameState.human.GetInventory().size();
+        gameSave << itemsHuman << endl;
+        for (int i : gameState.human.GetInventory())
+        {
+            gameSave << i << endl;
+        }
+        gameSave << gameState.human.GetSaws() << endl;
+        gameSave << gameState.human.GetBeers() << endl;
+        gameSave << gameState.human.GetMagnifiers() << endl;
+        gameSave << gameState.human.GetInverters() << endl;
+        gameSave << gameState.human.GetHandCuffs() << endl;
+        gameSave << gameState.human.GetCellPhones() << endl;
+
         gameSave << gameState.computer.GetHP() << endl;
+        int itemsComputer = gameState.computer.GetInventory().size();
+        gameSave << itemsComputer << endl;
+        for (int i : gameState.computer.GetInventory())
+        {
+            gameSave << i << endl;
+        }
+        gameSave << gameState.computer.GetSaws() << endl;
+        gameSave << gameState.computer.GetBeers() << endl;
+        gameSave << gameState.computer.GetMagnifiers() << endl;
+        gameSave << gameState.computer.GetInverters() << endl;
+        gameSave << gameState.computer.GetHandCuffs() << endl;
+        gameSave << gameState.computer.GetCellPhones() << endl;
+
         gameSave << gameState.magazine.ShowFull() << endl;
         gameSave << gameState.magazine.ShowEmpty() << endl;
         gameSave << gameState.magazine.ShowBulletCount() << endl;
+
         gameSave << gameState.gameStateManager.GetStarter() << endl;
         gameSave << gameState.ai.GetDifficulty() << endl;
         gameSave << gameState.gameStateManager.GetChoice() << endl;
+        gameSave << gameState.gameStateManager.GetShooter() << endl;
+        gameSave << gameState.gameStateManager.GetTarget() << endl;
+        gameSave << gameState.gameStateManager.GetItem() << endl;
+        gameSave << gameState.gameStateManager.GetDamage() << endl;
+        gameSave << gameState.gameStateManager.GetStateOfHandCuffs() << endl;
 
         for (int i : gameState.magazine.GetMagazine())
             gameSave << i << endl;
